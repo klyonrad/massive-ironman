2950a3179e1d5ac3ef31f78510063975f9f1a54d
KLuka <klukasocial+github@gmail.com>
2014-04-07 23:09:03 +0200

    initial commit

---
+++ openGLhomework/teil_1.cpp
@@ -0,0 +1,59 @@
+// GD-Praktikum:   teil_1.cpp  (Teil 1: Start-Programm)
+// Hergenroether / Groch    Last Update: 29.07.2011
+
+#include <iostream> 
+#include <GL/freeglut.h>      //lädt alles für OpenGL
+
+void Init()	
+{
+   // Hier finden jene Aktionen statt, die zum Programmstart einmalig 
+   // durchgeführt werden müssen
+}
+
+void RenderScene() //Zeichenfunktion
+{
+   // Hier befindet sich der Code der in jedem Frame ausgefuehrt werden muss
+   glLoadIdentity ();   // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
+   glBegin( GL_POLYGON );	
+      glColor4f( 1.0, 0.0, 0.0, 1.0);
+      glVertex3f( -0.5, -0.5, -0.5 );
+      glVertex3f(  0.5, -0.5, -0.5 );
+      glVertex3f(  0.5,  0.5, -0.5 );
+      glVertex3f( -0.5,  0.5, -0.5 );
+   glEnd();
+   glFlush(); //Buffer leeren   
+}
+
+void Reshape(int width,int height)
+{
+   // Hier finden die Reaktionen auf eine Veränderung der Größe des 
+   // Graphikfensters statt
+}
+
+void Animate (int value)    
+{
+   // Hier werden Berechnungen durchgeführt, die zu einer Animation der Szene  
+   // erforderlich sind. Dieser Prozess läuft im Hintergrund und wird alle 
+   // 1000 msec aufgerufen. Der Parameter "value" wird einfach nur um eins 
+   // inkrementiert und dem Callback wieder uebergeben. 
+   std::cout << "value=" << value << std::endl;
+   // RenderScene aufrufen
+   glutPostRedisplay();
+   // Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
+   glutTimerFunc(100, Animate, ++value);          
+}
+
+int main(int argc, char **argv)
+{
+   glutInit( &argc, argv );                // GLUT initialisieren
+   glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );        
+   glutInitWindowSize( 600, 600 );         // Fenster-Konfiguration
+   glutCreateWindow( "Name_1; Name_2" );   // Fenster-Erzeugung
+   glutDisplayFunc( RenderScene );         // Zeichenfunktion bekannt machen
+   glutReshapeFunc( Reshape );
+   // TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0  
+   glutTimerFunc( 10, Animate, 0);
+   Init();
+   glutMainLoop();
+   return 0;
+}
