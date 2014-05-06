eab6506146fab945208386ae608257331b8e9584
KLuka <klukasocial+github@gmail.com>
2014-05-06 16:04:14 +0200

    ein bisschen Klick-Klack

--- openGLhomework/rumspielerei.cpp
+++ openGLhomework/rumspielerei.cpp
@@ -1,145 +1,158 @@
-﻿// GD-Praktikum:   teil_1.cpp  (Teil 1: Start-Programm)
+﻿// GD-Praktikum:  kugeln.cpp by Jannis
 // Hergenroether / Groch    Last Update: 29.07.2011
 
 #include <iostream> 
-#include <GL/freeglut.h>      //lädt alles für OpenGL
-#include "wuerfel.hpp"
+#include <cstdlib>
+#include <math.h>
+#include <GL/freeglut.h>         //lädt alles für OpenGL
 
-float fRotation = 0.0;		// Rotation for robotarm
-bool down = true;
-float fullRotation = 0;
+#define PI 3.141592654
 
-void Init()	
-{
-	// Hier finden jene Aktionen statt, die zum Programmstart einmalig
-	// durchgeführt werden müssen
-	glEnable(GL_DEPTH_TEST);
-	glClearDepth(1.0);
+int angle=-75;
+int factor=2;
+int circle=0;
+int camcircle=0;
+float dist=500;
+
+
+void boden() { // completely fromjannis
+	  glPushMatrix(); 
+	  glColor4f(0.5,0.5,0.5,1.0);
+	  glBegin(GL_POLYGON);
+	  glVertex3f(-10,-8.5,500);
+	  glVertex3f(10,-8.5,500);
+	  glVertex3f(10,-8.5,-500);
+	  glVertex3f(-10,-8.5,-500);
+	  glEnd();
+	  glPopMatrix();
 }
 
-void RenderScene() //Zeichenfunktion
-{
-	// Hier befindet sich der Code der in jedem Frame ausgefuehrt werden muss
-	glLoadIdentity ();   // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
-
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		//Erweiterung für die Z -Achse
-	glClearColor( 1., 0.5, 0.0, 1.0 );
-
-	glTranslatef(-0.4, 1.8, 1.2); // verschieben
-	glutWireCube(0.2);
-	glLoadIdentity();
-
-	{ // Drehung WireCubes:
-		glTranslatef(0.1, 0.1, 0);
-		glRotatef( fullRotation, 1, 1, 0);
-		glTranslatef(0.1, -0.1, 0);
+void kugelgebilde() {
+	glPushMatrix();
+	{
+	  glColor4f(0.0,1.,0.,0.75);	  
+	  glutSolidSphere(0.1,10,10);	  
+	  glTranslatef(0.0,-1,0.0);
+  	  glutSolidSphere(0.1,10,10);
+  	  glTranslatef(0.0,-1,0.0);
+  	  glutSolidSphere(0.1,10,10);
+  	  glTranslatef(0.0,-1,0.0);
+  	  glutSolidSphere(0.1,10,10);
+	  glTranslatef(0.0,-1.25,0.0);
+	  glutSolidSphere(0.5,100,100);
 	}
+	glPopMatrix();
+}
 
-	glTranslatef(0, -1, 0);
-
-	glutWireCube(0.4);
-	glutWireCube(0.6);
-	glutWireCube(0.8);
-	glutWireCube(1);
-	glutWireCube(1.2);
-	glLoadIdentity();
-
-	//Ablauf von unten nach oben
-
-	{// Roboterarm
-		glTranslatef(-0.4, 1.8, 1.2); // verschieben
-		// Drehung:
-		glTranslatef(0.1, 0.1, 0);
-		glRotatef( fRotation, 0, 0, 1);			//Ohne Push und Pop kann ich auch beide hier drehen und spare 1 Zeile / Operation
-		glTranslatef(0.1, -0.1, 0);
-		
+void kugelReihe(int breite) { // laaaaaag
+	for (int i = -breite; i <= breite; i++) {
 		glPushMatrix();
+		glTranslatef(i, 0, 0);
+		kugelgebilde();
+		glPopMatrix();
 	}
+}
 
-	//Oberarm
-	glTranslatef(0.8,0,0);
-	glScalef(1.5, 0.25, 0.25);
-	Wuerfel(0.4);
+void Init()	
+{  
+   glEnable(GL_DEPTH_TEST);
+   glClearDepth(1.0);
+}
+
+void RenderScene()
+{
+   glLoadIdentity ();
+   	  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+   	  
+   	  glEnable(GL_BLEND);
+   	  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+   	     	  
+   	  glClearColor(0.,0.,1.0,1.0);
+	  gluLookAt(0.0,-8.5+5,15.,0.,0.,0.,0.,1.,0.);
+	  
+	  // glRotatef(-10.0,0.0,1.0,0.0);
+	  
+	  glPushMatrix();
+	  
+	  //glTranslatef(0.0, sin(circle*PI/180)+1 ,0.0); // Das rollende Ding soll sich auf und ab bewegen
+	  
+	  glPushMatrix();
+	  
+		glTranslatef(-5, 0, 0); // Kugelgebilde nach links verschieben
+		glRotatef(angle,0.0,0.0,1.0);
+		kugelgebilde();
+		
+
+
+	glPopMatrix();
+		glTranslatef(5, 0, 0); // Kugelgebilde nach rechts verschieben
+		glRotatef(-angle,0.0,0.0,1.0);
+		kugelgebilde();
 
 	glPopMatrix();
 
-	//Unterarm
-	glTranslatef(0.2,0,0);
-	glScalef(1.5, 0.5, 0.5);
-	Wuerfel(0.4);
+	kugelReihe(1);
 
-	glutSwapBuffers();
-	glFlush(); //Buffer leeren   
+
+
+
+	  
+	  glPushMatrix();
+	  glColor4f(1.0,0.5,0.0,0.3);
+	  glScalef(170.0,1.0,1.0);
+	  glutSolidCube(0.1);  // Stange
+	  glPopMatrix();
+	  
+	  
+	  glPopMatrix(); 
+	  boden();
+    
+	  glutSwapBuffers();	
+   glFlush(); //Buffer leeren
+
 }
 
 void Reshape(int width,int height)
 {
-	// Hier finden die Reaktionen auf eine Veränderung der Größe des 
-	// Graphikfensters statt
 	// Matrix für Transformation: Frustum->viewport
 	glMatrixMode(GL_PROJECTION);
 	// Aktuelle Transformations-Matrix zuruecksetzen
 	glLoadIdentity ();
 	// Viewport definieren
 	glViewport(0,0,width,height);
-	// Frustum definieren (siehe unten)
-
-	//11a:
-	glOrtho( -1.4, 1.4, -1.4, 1.4, 
-		0, 3);				//Größerer Sichtbereich
-	gluLookAt ( 1., 1., 1., 0., 0., 0., 0., 1., 0.);		//Betrachte den Würfel von Vorne Oben
-
-	// Matrix für Modellierung/Viewing
+    gluPerspective(80., 1., 0.1, 500.);	
 	glMatrixMode(GL_MODELVIEW);
 }
 
 void Animate (int value)    
-{
-	// Hier werden Berechnungen durchgeführt, die zu einer Animation der Szene  
-	// erforderlich sind. Dieser Prozess läuft im Hintergrund und wird alle 
-	// 100 msec aufgerufen. Der Parameter "value" wird einfach nur um eins 
-	// inkrementiert und dem Callback wieder uebergeben. 
-	std::cout << "value=" << value << std::endl;
+{  
+   // RenderScene aufrufen
+   glutPostRedisplay();
+   
+   circle=(circle+8) % 360;   
+   camcircle=(camcircle+1) % 360;   
 
-	//Realistische Rotation um 175°
-	if(down){
-		if(fRotation == -85.0)
-			down = false;
-		fRotation -= 0.5;
-	}
-	else{
-		if(fRotation == 90.0)
-			down = true;
-		fRotation += 0.5;
-	}
+   dist+=0.75;
+   dist=(dist >10) ? -500 : dist;
 
-	fullRotation += 1;
-	//Originale Rotation 360°
-	/*
-	fRotation = fRotation - 1.0; // Rotationswinkel aendern
-	if ( fRotation <= 0.0) {
-	fRotation = fRotation + 360.0;
-	}*/
-
-
-	// RenderScene aufrufen 
-	glutPostRedisplay();
-
-	// Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
-	glutTimerFunc(10, Animate, ++value);          
+   angle=angle+factor;
+   if(angle==1 || angle==-91) // muss ungerade sein, da factor gerade ist
+	   factor=factor*-1;
+   // Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
+   glutTimerFunc(10, Animate, ++value);          
 }
 
 int main(int argc, char **argv)
 {
-	glutInit( &argc, argv );                // GLUT initialisieren
-	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB |GLUT_DEPTH);      //Erweiterung für die Z Achse  
-	glutInitWindowSize( 600, 600 );         // Fenster-Konfiguration
-	glutCreateWindow( "Luka; Max" );   // Fenster-Erzeugung
-	glutDisplayFunc( RenderScene );         // Zeichenfunktion bekannt machen
-	glutReshapeFunc( Reshape );
-	// TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0  
-	glutTimerFunc( 10, Animate, 0);
-	Init();
-	glutMainLoop();
-	return 0;
+   glutInit( &argc, argv );                // GLUT initialisieren
+   glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
+   glutInitWindowSize( 600, 600 );         // Fenster-Konfiguration
+   glutCreateWindow( "kette" );   // Fenster-Erzeugung
+   glutDisplayFunc( RenderScene );         // Zeichenfunktion bekannt machen
+   glutReshapeFunc( Reshape );
+   // TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0  
+   glutTimerFunc(10, Animate, 0);
+   Init();
+   glutMainLoop();
+   return 0;
 }
